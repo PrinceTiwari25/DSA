c7b35d267e7e0aeac9f334df96fc978bedd3968e
PRINCE TIWARY <199446200+PrinceTiwari25@users.noreply.github.com>
2026-09-04 19:18:46 +0530

    Refactor threeSum method for improved readability

--- 15-three sum/3sum.java
+++ 15-three sum/3sum.java
@@ -1,55 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         List<List<Integer>> ans = new ArrayList<>();
-
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // Skip duplicate starting values
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        for(int i=0;i<nums.length-1;i++){
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
+            int j=i+1;
+            int  k=nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
                     ans.add(Arrays.asList(
-                        nums[i],
-                        nums[left],
-                        nums[right]
+                        nums[i],nums[j],nums[k]
                     ));
 
-                    left++;
-                    right--;
-
-                    // Skip duplicate left values
-                    while (left < right &&
-                           nums[left] == nums[left - 1]) {
-                        left++;
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
                     }
-
-                    // Skip duplicate right values
-                    while (left < right &&
-                           nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+                    
                 }
             }
         }
-
         return ans;
     }
 }
